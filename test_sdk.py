4777020d7bf028c23975ce161cea9330ed0d1068
flkuo <fkuo2@wisc.edu>
2025-01-30 22:51:58 +0800

    Add missing requirements.txt

--- test_sdk.py
+++
@@ -1,6 +0,0 @@
-from linebot.v3.messaging import MessagingApi, ApiClient
-from linebot.v3.webhook import WebhookHandler
-from linebot.v3.webhook.event import MessageEvent
-from linebot.v3.webhook.message import TextMessageContent
-
-print("LINE Bot SDK imports work!")
