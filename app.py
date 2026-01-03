dd76b9238e2d7e3ac75e521242ff24e5e6df7bfa
flkuo <dajotter@gmail.com>
2026-01-03 22:49:06 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = database_url or 'sqlite:///' + os.path.j
 app.config['UPLOAD_FOLDER'] = os.path.join(basedir, 'static', 'uploads')
 
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
